f9d208acdb7e48fe003a447c43bbb5315d7ba38e
unknown <chihab.che@gmail.comgit config --global user.name>
2022-08-11 23:51:52 +0100

    my third

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Form from './Form';
 import Todolist from './Todolist';
 
 function App() {
-
+// state stuff
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filtereTodos, setFiltereTodos] = useState([]);
+
+// effect
+
+useEffect(() => {
+  filterHnadler()
+}, [todos])
+
+
+  //fUNCTION
+
+const filterHnadler = () => {
+  switch(status){
+    case 'completed' :
+      setFiltereTodos(todos.filter(todo => todo.completed = true));
+      break;
+     case 'incompleted' : 
+     setFiltereTodos(todos.filter(todo => todo.completed = false));
+      break;
+      default :
+      setFiltereTodos(todos)
+  }
+}
 
   return (
     <div className="App">
       <header>
       <h1>My Todo List</h1>
     </header>
-    <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-    <Todolist todos={todos} setTodos={setTodos}/>
+    <Form inputText={inputText} 
+    todos={todos} 
+    setTodos={setTodos} 
+    setInputText={setInputText} 
+    status={status} 
+    setStatus={setStatus} />
+    <Todolist todos={todos} setTodos={setTodos} filtereTodos={filtereTodos}/>
     </div>
   );
 }
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Todo from './Todo'
 
-const Todolist = ({todos, setTodos}) => {
+const Todolist = ({todos, setTodos, filtereTodos}) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map(todo =>(
+        {filtereTodos.map(todo =>(
             <Todo 
                 setTodos={setTodos}
                 todos={todos}
+                todo={todo}
                 text={todo.text} 
                 key={todo.id}/>
         ))}
